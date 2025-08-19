6cd6823e5b94dd4a9a9f319dfe5faa5d65e4a377
Rositsa Nenova <rositsa.a.nenova@gmail.com>
2025-08-19 07:24:27 +0300

    c1

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-document.getElementById('apply-btn').addEventListener('click', function() {
-  var color = document.getElementById('color-input').value;
-  document.getElementById('box').style.backgroundColor = color;
+document.getElementById('apply-btn').addEventListener('click',function(){
+  var c=document.getElementById('color-input').value;
+  document.getElementById('box').style.backgroundColor=c;
 });
+
