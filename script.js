1107cff8e6fc04a68112119ec21303374aa625f9
Rositsa Nenova <rositsa.a.nenova@gmail.com>
2025-08-19 07:24:41 +0300

    c2

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ document.getElementById('apply-btn').addEventListener('click',function(){
   document.getElementById('box').style.backgroundColor=c;
 });
 
+function setBox(c){document.getElementById('box').style.backgroundColor=c;}
+function randomHex(){return'#'+Math.floor(Math.random()*0xffffff).toString(16).padStart(6,'0');}
+
+
