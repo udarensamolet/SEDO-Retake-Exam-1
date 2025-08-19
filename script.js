f13a0a706f513d958e3ced33718b3bb17384618d
Rositsa Nenova <rositsa.a.nenova@gmail.com>
2025-08-19 07:32:25 +0300

    c3

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ const input = $('#color-input');
 $('#apply-btn').addEventListener('click', () => {
   box.style.backgroundColor = input.value;
 });
+
+function log(msg) {
+  console.log('[app]', msg);
+}
+
+function shortHex(h) {
+  return /^#([0-9a-f]{6})$/i.test(h) && h[1] == h[2] && h[3] == h[4] && h[5] == h[6] ?
+    '#' + h[1] + h[3] + h[5] : h;
+}
+document.addEventListener('DOMContentLoaded', () => log('ready'));
